6fc10fafd47b7da3f29a6b2d2d20d0e1a299ff61
ashishsinglaa <73412453+ashishsinglaa@users.noreply.github.com>
2024-02-15 20:02:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+const productRouter = require("./routes/product");
+const authRouter = require("./routes/auth");
+const cartRouter = require("./routes/cart");
+
+const app = express();
+
+dotenv.config();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/product", productRouter);
+app.use("/auth", authRouter);
+app.use("/cart", cartRouter);
+
+mongoose.connect(process.env.MONGO_DB_URL);
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on PORT ${process.env.PORT}`);
+});
